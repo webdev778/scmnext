be107437dde92d5ec23dffda4a731c2d92489ae4
Toshihisa KATO <toshihk@gmail.com>
2019-02-15 19:15:52 +0900

    確定値の取り込み処理を修正

--- backend/app/models/power_usage_fixed.rb
+++ backend/app/models/power_usage_fixed.rb
@@ -37,14 +37,28 @@ class PowerUsageFixed < ApplicationRecord
       supply_point_number_map = SupplyPoint.get_map_filter_by_compay_id_and_district_id(company_id, district_id)
 
       setting.get_xml_object_and_process_high_and_low(:fixed, date) do |doc, voltage_class|
+        jptrm = doc.elements['SBD-MSG/JPMGRP/JPTRM']
         import_data = jptrm.elements['JPM00010'].to_a.map do |nodes_by_facility|
           next if nodes_by_facility.elements['JP06405'].text != '0'
           supply_point_number = nodes_by_facility.elements['JP06400'].text
+          journal_code = nodes_by_facility.elements['JP06404'].text # 仕訳コード
           supply_point = supply_point_number_map[supply_point_number]
           if supply_point.nil?
             logger.error("供給地点特定番号:[#{supply_point_number}]に対応する施設が見つかりません。需要家名=#{nodes_by_facility.elements['JP06120'].text}")
             puts "供給地点特定番号:[#{supply_point_number}]に対応する施設が見つかりません。需要家名=#{nodes_by_facility.elements['JP06120'].text}"
             next
+          end
+          value_tag = case journal_code
+          when '1'
+            'JP06424'
+          when '2'
+            'JP06425'
+          else
+            raise "不明な仕訳コード:#{journal_code}"
+          end
+          if journal_code == '2' and not(supply_point.supply_method_type_partial?)
+            logger.error("供給地点特定番号:[#{supply_point_number}]は供給区分とxml上の仕訳コードが一致しません。")
+            puts "供給地点特定番号:[#{supply_point_number}]は供給区分とxml上の仕訳コードが一致しません。"
           end
           nodes_by_facility.elements['JPM00013'].to_a.map do |nodes_by_day|
             date = Time.strptime(nodes_by_day.elements['JP06423'].text, "%Y%m%d")
@@ -53,17 +67,12 @@ class PowerUsageFixed < ApplicationRecord
               puts "供給地点特定番号:[#{supply_point_number}]は契約期間外です。需要家名=#{nodes_by_facility.elements['JP06120'].text}"
               next
             end
-            value = BigDecimal(nodes_by_time.elements['JP06424'].text)
-            if setting.district.is_partial_included and supply_point.supply_method_type_partial? then
-              value = value - (supply_point.base_power / 2)
-              value = value >= 0 ? value : 0
-            end
             nodes_by_day.elements['JPM00014'].to_a.map do |nodes_by_time|
               {
                 date: date,
                 time_index_id: nodes_by_time.elements['JP06219'].text,
                 facility_group_id: supply_point.facility_group.id,
-                value: value
+                value: BigDecimal(nodes_by_time.elements[value_tag].text)
               }
             end
           end
