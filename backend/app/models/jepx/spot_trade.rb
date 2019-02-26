a6b97b319776385e5b6024edd8fcd01e46960b0e
baito1 <baito1@optimizer.co.jp>
2019-02-26 15:18:16 +0900

    ENEPASCM_2-9 JEPXスポット市場取引結果取込

--- backend/app/models/jepx/spot_trade.rb
+++ backend/app/models/jepx/spot_trade.rb
@@ -24,9 +24,63 @@
 class Jepx::SpotTrade < ApplicationRecord
   has_many :spot_trade_area_data
 
+  accepts_nested_attributes_for :spot_trade_area_data
+
+  require 'csv'
+
   class << self
-    def import(date = nil)
-      puts "download!"
+    def import_data(year)
+      conn = Faraday::Connection.new(:url => 'http://www.jepx.org') do |builder|
+        builder.use Faraday::Request::UrlEncoded
+        builder.use Faraday::Adapter::NetHttp
+      end
+      response = conn.get('market/excel/spot_' + year.to_s + '.csv')
+      csv = CSV.parse(response.body, headers: true)
+      (0..csv.length - 1).each do |i|
+        area = []
+        same_datetime_record = self.find_by(date: csv[i][0], time_index_id: csv[i][1])
+        if same_datetime_record
+          area_data_id = same_datetime_record.spot_trade_area_data.first.id
+          (1..9).each do |j|
+            area << {id: (area_data_id + j - 1), area_price: csv[i][j + 5], avoidable_price: csv[i][j + 22]}
+          end
+          row = {
+            sell_bit_amount: csv[i][2],
+            buy_bit_amount: csv[i][3],
+            execution_amount: csv[i][4],
+            system_price: csv[i][5],
+            avoidable_cost: csv[i][22],
+            spot_avg_per_price: csv[i][16],
+            alpha_max_times_spot_avg_per_price: csv[i][17],
+            alpha_min_times_spot_avg_per_price: csv[i][18],
+            alpha_preliminary_times_spot_avg_per_price: csv[i][19],
+            alpha_fixed_times_spot_avg_per_price: csv[i][20],
+            spot_trade_area_data_attributes: area
+          }
+          same_datetime_record.update(row)
+        else
+          (1..9).each do |j|
+            district = District.find_by(code: "0#{j}")
+            area << {district_id: district.id, area_price: csv[i][j + 5], avoidable_price: csv[i][j + 22]}
+          end
+          row = {
+            date: csv[i][0],
+            time_index_id: csv[i][1],
+            sell_bit_amount: csv[i][2],
+            buy_bit_amount: csv[i][3],
+            execution_amount: csv[i][4],
+            system_price: csv[i][5],
+            avoidable_cost: csv[i][22],
+            spot_avg_per_price: csv[i][16],
+            alpha_max_times_spot_avg_per_price: csv[i][17],
+            alpha_min_times_spot_avg_per_price: csv[i][18],
+            alpha_preliminary_times_spot_avg_per_price: csv[i][19],
+            alpha_fixed_times_spot_avg_per_price: csv[i][20],
+            spot_trade_area_data_attributes: area
+          }
+          self.create(row)
+        end
+      end
     end
   end
 end
--- backend/lib/tasks/jepx.rake
+++ backend/lib/tasks/jepx.rake
@@ -8,8 +8,17 @@ namespace :jepx do
 
     desc "JEPXスポット市場取引結果取込"
     task spot_trade: :environment do |task, args|
-      date = determine_target_date(nil)
-      Jepx::SpotTrade.import(date)
+      year = ENV['YEAR'] ? ENV['YEAR'].to_i : nil
+      target_date = Date.today
+      if year
+        Jepx::SpotTrade.import_data(year)
+      elsif [4, 5].include? target_date.month and Jepx::SpotTrade.where(date: Date(target_date.year, 3, 31)).exists?
+        Jepx::SpotTrade.import_data(target_date.year)
+        Jepx::SpotTrade.import_data(target_date.year - 1)
+      else
+        target_year = target_date.month >= 4 ? target_date.year : target_date.year - 1
+        Jepx::SpotTrade.import_data(target_year)
+      end
     end
   end
 end
