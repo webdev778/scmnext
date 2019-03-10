7ad4a0f9caad5f2b973b71aaf0b8100f6c94fdfc
Toshihisa KATO <toshihk@gmail.com>
2019-03-10 14:07:04 +0900

    add whenever

---
+++ backend/config/schedule.rb
@@ -0,0 +1,42 @@
+# Use this file to easily define all of your cron jobs.
+#
+# It's helpful, but not entirely necessary to understand cron before proceeding.
+# http://en.wikipedia.org/wiki/Cron
+
+# Example:
+#
+# set :output, "/path/to/my/cron_log.log"
+#
+# every 2.hours do
+#   command "/usr/bin/some_great_command"
+#   runner "MyModel.some_method"
+#   rake "some:great:rake:task"
+# end
+#
+# every 4.days do
+#   runner "AnotherModel.prune_old_records"
+# end
+
+# docker環境を前提に書き換える
+docker_command = "cd /home/ubuntu/scmnext && sudo docker-compose exec backend"
+job_type :rake,    "#{docker_command} :environment_variable=:environment bundle exec rake :task --silent :output"
+job_type :runner,  "#{docker_command} bin/rails runner -e :environment ':task' :output"
+job_type :script,  "#{docker_command} :environment_variable=:environment bundle exec script/:task :output"
+
+# 託送データのダウンロードと速報値当日データの取り込み
+every 10.minute do # 1.minute 1.day 1.week 1.month 1.year is also supported
+  rake 'dlt:download'
+  rake 'dlt:import:today'
+end
+
+# 速報値過去データの取り込み
+every 1.day, at: '9:00' do
+  rake 'dlt:import:past'
+end
+
+# 確定値の取り込み
+every 1.day, at: '9:00' do
+  rake 'dlt:import:fixed'
+end
+
+# Learn more: http://github.com/javan/whenever
