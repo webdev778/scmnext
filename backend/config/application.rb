9d1da7eda42ffa62b3851f0f84b17e0096dbefe8
Toshihisa KATO <toshihk@gmail.com>
2019-03-22 10:36:47 +0900

    タイムゾーンをJSTに

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -26,6 +26,7 @@ module EnepaSCM
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.time_zone = 'Tokyo'
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
