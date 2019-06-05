cdd7de103eab0d4f8f12d7d0e63eda2b5541bd22
Toshihisa KATO <toshihk@gmail.com>
2019-06-05 16:34:50 +0900

    annotate

--- backend/app/models/resource.rb
+++ backend/app/models/resource.rb
@@ -2,16 +2,19 @@
 #
 # Table name: resources
 #
-#  id           :bigint(8)        not null, primary key
-#  bg_member_id :bigint(8)
-#  type         :string(255)      not null
-#  code         :string(255)      not null
-#  name         :string(255)      not null
-#  supply_value :text(65535)
-#  created_by   :integer
-#  updated_by   :integer
-#  created_at   :datetime         not null
-#  updated_at   :datetime         not null
+#  id              :bigint(8)        not null, primary key
+#  bg_member_id    :bigint(8)
+#  type            :string(255)      not null
+#  code            :string(255)      not null
+#  name            :string(255)      not null
+#  contract_number :string(20)
+#  max_value       :decimal(14, )    default(0), not null
+#  min_value       :decimal(14, )    default(0), not null
+#  unit            :decimal(14, )    default(1), not null
+#  created_by      :integer
+#  updated_by      :integer
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
 #
 
 class Resource < ApplicationRecord
--- backend/test/models/power_generator_test.rb
+++ backend/test/models/power_generator_test.rb
@@ -2,16 +2,16 @@
 #
 # Table name: power_generators
 #
-#  id                       :bigint(8)        not null, primary key
-#  power_generator_group_id :bigint(8)
-#  code                     :string(5)        not null
-#  name                     :string(255)      not null
-#  contract_number          :string(20)
-#  supply_max               :integer
-#  created_by               :integer
-#  updated_by               :integer
-#  created_at               :datetime         not null
-#  updated_at               :datetime         not null
+#  id              :bigint(8)        not null, primary key
+#  resources_id    :bigint(8)
+#  code            :string(5)        not null
+#  name            :string(255)      not null
+#  contract_number :string(20)
+#  supply_max      :integer
+#  created_by      :integer
+#  updated_by      :integer
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
 #
 
 require 'test_helper'
